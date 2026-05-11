9844700fc188061c54f7a5d5825af7e16b62798b
Ryan Badger <ryan@clickheredigital.co.uk>
2026-05-11 18:24:38 +0800

    Update storageto to v0.3.2

--- Formula/storageto.rb
+++ Formula/storageto.rb
@@ -4,28 +4,28 @@
 class Storageto < Formula
   desc "CLI tool for storage.to - simple file sharing"
   homepage "https://github.com/storageto/cli"
-  version "0.3.0"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/storageto/cli/releases/download/v#{version}/storageto-darwin-arm64.tar.gz"
-      sha256 "7fbbbe28af63bae8999e3a36773b5d299caebd34a8fde9087c715564a9869534"
+      sha256 "eee0400a76be82101ea0fe4c455c4865c47de0b60d314c139bd18c82f2f50eea"
     end
     on_intel do
       url "https://github.com/storageto/cli/releases/download/v#{version}/storageto-darwin-amd64.tar.gz"
-      sha256 "fcb39facf9092122e9a9b3355285f1893d5ffd0bc3570d502ebdfb44cac0e096"
+      sha256 "583081ad5189063ae1138a90ae7d72ab491364f2de28cfbc8c62d4fc22eebb90"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/storageto/cli/releases/download/v#{version}/storageto-linux-arm64.tar.gz"
-      sha256 "26e446cd6d3b1e16ba7b1f636f6101d51cdf3390da1a70925c167e9db920c439"
+      sha256 "ad79700699a828d0135d87077fbd9bd0970987e047c316f0f72fea5633a5890c"
     end
     on_intel do
       url "https://github.com/storageto/cli/releases/download/v#{version}/storageto-linux-amd64.tar.gz"
-      sha256 "ccef8f6d4eda53f5dc5cfc557829e4980483f773b22c090cf237cc44f6df362d"
+      sha256 "225df3400ce81d9362fbd34c868bc8e828508f69b8543735ad4e35045461cdb6"
     end
   end
 
